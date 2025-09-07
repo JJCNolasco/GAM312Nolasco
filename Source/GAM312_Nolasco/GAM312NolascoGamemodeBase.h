1675ffb0acc42687e7754042e2bdbfb576a9ca30
HM2 Joseph Nolasco <jjcnolasco777@gmail.com>
2025-09-07 01:32:53 -0700

    Started Player Setup

---
+++ Source/GAM312_Nolasco/GAM312NolascoGamemodeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAM312NolascoGamemodeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_NOLASCO_API AGAM312NolascoGamemodeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
