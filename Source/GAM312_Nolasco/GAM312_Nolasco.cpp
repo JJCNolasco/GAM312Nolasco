5a920afbf53f2fdd324079189c657ad4b613f32c
HM2 Joseph Nolasco <jjcnolasco777@gmail.com>
2025-09-07 00:10:00 -0700

    Initial Commit

---
+++ Source/GAM312_Nolasco/GAM312_Nolasco.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312_Nolasco.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312_Nolasco, "GAM312_Nolasco" );
